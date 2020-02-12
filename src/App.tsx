b3bb7e7ebaca3637ae6e94217281e0d462a33fa5
Tatsuya Watanabe <blancnoir.tw@gmail.com>
2020-02-12 17:37:56 +0900

    Add GlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
+import GlobalStyle from './GlobalStyle'
+
 const App = () => {
-  return <div>APP</div>
+  return (
+    <>
+      <GlobalStyle />
+      <div>APP</div>
+    </>
+  )
 }
 
 export default App
---
+++ src/GlobalStyle.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Global, css } from '@emotion/core'
+
+const GlobalStyle = () => (
+  <Global
+    styles={css`
+      body {
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell',
+          'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+      }
+
+      code {
+        font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
+      }
+    `}
+  />
+)
+
+export default GlobalStyle
